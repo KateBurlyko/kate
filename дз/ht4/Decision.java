4b69dfe35f44cf0e080e23d3982061f31465b8ec
Kate <kateeds69@gmail.com>
2016-10-05 00:27:26 +0300

    add 4

---
+++ дз/ht4/Decision.java
@@ -0,0 +1,41 @@
+package ht4;
+
+/**
+ * it has method Decision
+ */
+public class Decision {
+    /*
+   * counting roots of task
+     *
+     * @param a first number
+     * @param b second number
+     * *@param c second number
+     * @return nothing
+     */
+    public void decision(double a, double b, double c) {
+        double d = b * b - 4 * a * c;
+
+        if (1. / a == Double.POSITIVE_INFINITY || 1. / a == Double.NaN) {
+            System.out.print("This task does not have roots");
+            return;
+        }
+
+        if (d < 0) {
+            System.out.println("This task does not have roots");
+            return;
+        }
+        if (1. / d == Double.POSITIVE_INFINITY || 1. / d == Double.NaN) {
+            double x1;
+            x1 = (-1 * b) / (2 * a);
+            System.out.println("root: x1=x2= " + x1);
+            return;
+        } else {
+            double x1 = ((-1 * b) + Math.sqrt(d)) / (2 * a);
+            double x2 = ((-1 * b) - Math.sqrt(d)) / (2 * a);
+            System.out.println("roots: x1= " + x1 + ",   " + "x2= " + x2);
+            return;
+        }
+
+    }
+}
+
---
+++ дз/ht4/Main.java
@@ -0,0 +1,42 @@
+package ht4;
+
+import java.util.Scanner;
+/*
+this class has method main
+ */
+
+public class Main {
+/*
+class main inputs number and method decision
+ */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+
+        try {
+            //catch exeption
+            do {
+                System.out.print("Input a:");
+                a = in.nextDouble();
+                System.out.print("Input b:");
+                b = in.nextDouble();
+                System.out.print("Input c:");
+                c = in.nextDouble();
+            }
+         while (a < Double.MIN_VALUE || a > Double.MAX_VALUE ||
+                    b<Double.MIN_VALUE ||b >Double.MAX_VALUE ||
+                    c<Double.MIN_VALUE ||c >Double.MAX_VALUE) ;
+
+            /*
+create class object and call the method decision
+ */
+            Decision decis = new Decision();
+            decis.decision(a,b,c);
+    }
+    catch(java.util.InputMismatchException e){
+        System.exit(5);
+    }
+}
+}
