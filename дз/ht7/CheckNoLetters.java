79160ffc4d49ed23b642108e529dfd680857d391
Kate <kateeds69@gmail.com>
2016-10-10 14:10:18 +0300

    ad

--- дз/ht7/CheckNoLetters.java
+++ дз/ht7/CheckNoLetters.java
@@ -8,16 +8,20 @@ import java.util.Scanner;
 public class CheckNoLetters {
 
     /**
-     * The method checks letters on line and if there are no letters say about it.
+     * The method checks letters on line and if there are only numbers say about it.
      *
      * @param line string which user input.
      */
     public void checkNoLetters(String line) {
         Scanner scanner = new Scanner(line);
 
-        if(scanner.hasNextInt()){
+        if(scanner.hasNextInt() && line.matches(" ")){
             System.out.println("String corresponds to rule 2: contains only numbers");
         } else{
         }
+        /*if (line.matches("\\d+")) {
+            System.out.println("String corresponds to rule 2: contains only numbers");
+        } else {
+        }*/
     }
 }
