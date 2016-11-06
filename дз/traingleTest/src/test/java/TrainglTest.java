60247df5d7d6248f99276aa8e97ac1e586bd25a9
Kate <kateeds69@gmail.com>
2016-11-06 22:55:22 +0300

    add changes

--- дз/traingleTest/src/test/java/TrainglTest.java
+++ дз/traingleTest/src/test/java/TrainglTest.java
@@ -10,36 +10,41 @@ public class TrainglTest {
     @DataProvider(name = "sides")
     public Object[][] numbers() {
         return new Object[][]{
-                {new Double(NaN), new Double(5), new Double(5)},
-                {new Double(5), new Double(NaN), new Double(5)},
-                {new Double(5), new Double(5), new Double(NaN)},
-                {new Double(NaN), new Double(NaN), new Double(NaN)},
-                {new Double(NEGATIVE_INFINITY), new Double(5), new Double(5)},
-                {new Double(8), new Double(NEGATIVE_INFINITY), new Double(5)},
-                {new Double(5), new Double(5), new Double(NEGATIVE_INFINITY)},
-                {new Double(NEGATIVE_INFINITY), new Double(NEGATIVE_INFINITY), new Double(NEGATIVE_INFINITY)},
-                {new Double(POSITIVE_INFINITY), new Double(5), new Double(5)},
-                {new Double(8), new Double(POSITIVE_INFINITY), new Double(5)},
-                {new Double(8), new Double(5), new Double(POSITIVE_INFINITY)},
-                {new Double(POSITIVE_INFINITY), new Double(POSITIVE_INFINITY), new Double(POSITIVE_INFINITY)},
-                {new Double(-8), new Double(5), new Double(5)},
-                {new Double(5), new Double(-8), new Double(5)},
-                {new Double(5), new Double(5), new Double(-8)},
-                {new Double(0), new Double(0), new Double(0)},
+                {NaN, 5, 5},
+                {5, NaN, 5},
+                {5, 5, NaN},
+                {NaN, NaN, NaN},
+                {NEGATIVE_INFINITY, 5, 5},
+                {8, NEGATIVE_INFINITY, 5},
+                {5, 5, NEGATIVE_INFINITY},
+                {NEGATIVE_INFINITY, NEGATIVE_INFINITY, NEGATIVE_INFINITY},
+                {POSITIVE_INFINITY, 5, 5},
+                {8, POSITIVE_INFINITY, 5},
+                {8, 5, POSITIVE_INFINITY},
+                {POSITIVE_INFINITY, POSITIVE_INFINITY, POSITIVE_INFINITY},
+                {(-8), (5), (5)},
+                {(5), (-8), (5)},
+                {(5), (5), (-8)},
+                {(0), (0), (0)},
+                {Double.MAX_VALUE, (5), (5)}
         };
     }
 
     @DataProvider(name = "side")
     public Object[][] number() {
         return new Object[][]{
-                {"traingl is equilateral", new Double(5), new Double(5), new Double(5)},
-                {"traingl is usual", new Double(5.0000000000000011), new Double(5.0000000000000021), new Double(5.0000000000000031)},
-                {"traingle is isosceles", new Double(5), new Double(5), new Double(6)},
-                {"traingle is usual", new Double(5), new Double(6), new Double(7)},
-                {"traingle is usual", new Double(5.0000000000000000000001), new Double(4.999999999999999999999), new Double(7)},
-                {"traingle is usual", new Double(5.5), new Double(6.66), new Double(7.777)},
-                {"traingle is usual", new Double(6 / 2), new Double(6 / 3), new Double(7 / 4)},
-                {"traingl is equilateral", new Double(5 / 5), new Double(5 / 5), new Double(5 / 5)}
+                {"traingl is equilateral", (5), (5), (5)},
+                {"traingl is usual", (5.0000000000000011), (5.0000000000000021), (5.0000000000000031)},
+                {"traingle is isosceles", (5), (5), (6)},
+                {"traingle is usual", (5), (6), (7)},
+                {"traingle is usual", (5.0000000000000000000001), (4.999999999999999999999), (7)},
+                {"traingle is usual", (5.5), (6.66), (7.777)},
+                {"traingle is usual", (6 / 2), (6 / 3), (7 / 4)},
+                {"traingl is equilateral", (5 / 5), (5 / 5), (5 / 5)},
+                {"traingle is isosceles", Double.MIN_VALUE, (5 / 5), (5 / 5)},
+                {"traingle is isosceles", Double.MIN_VALUE, Double.MAX_VALUE, Double.MAX_VALUE},
+                {"traingl is equilateral", Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE},
+                {"traingl is equilateral", Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE}
         };
     }
 
